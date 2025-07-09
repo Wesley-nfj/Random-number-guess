0d80c929bc321348c860f06677225c967963b2e2
Wesley-nfj <wesleyfonkem7@icloud.com>
2025-07-09 16:07:54 +0100

    add input validation with attempt tracking

--- Random_num_guess.py
+++ Random_num_guess.py
@@ -1,12 +1,18 @@
 import random
 
 secret_number = random.randint(1, 10)
+attempts = 0
 
-guess = int(input("Guess a number between 1 and 10: \n"))
-if 1<= guess <= 10:
-  if guess == secret_number:
-    print("You got it! 🎉")
+try:
+  guess = int(input("Guess a number between 1 and 10: \n"))
+  if 1<= guess <= 10:
+    attempts +=1
+    if guess == secret_number:
+      print("You got it! 🎉")
+    else:
+        print(f"Oops! The correct number was {secret_number}. Try again!")
+        print(f"You have used {attempts} attempts")
   else:
-    print(f"Oops! The correct number was {secret_number}. Try again!")
-else:
-  print("Please enter a number from 1 to 10")
+      print("Please enter a number from 1 to 10")
+except ValueError:
+    print("Invalid input!!")
