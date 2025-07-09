d2d0bcf905dc09b2eabb11f1de739936d5c2435d
Wesley-nfj <wesleyfonkem7@icloud.com>
2025-07-09 15:09:39 +0100

    Fix typing error , handle invalid input and ensure guess is in valid range

--- Random_num_guess.py
+++ Random_num_guess.py
@@ -2,9 +2,11 @@ import random
 
 secret_number = random.randint(1, 10)
 
-guess = int(input("7Guess a number between 1 and 10: \n"))
-
-if guess == secret_number:
+guess = int(input("Guess a number between 1 and 10: \n"))
+if 1<= guess <= 10:
+  if guess == secret_number:
     print("You got it! 🎉")
-else:
+  else:
     print(f"Oops! The correct number was {secret_number}. Try again!")
+else:
+  print("Please enter a number from 1 to 10")
